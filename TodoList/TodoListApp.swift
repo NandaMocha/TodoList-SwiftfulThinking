b3af722e3a30643d54742c8f98892917ee052338
Nanda Mochammad <nanda.rosyadi@noble-software.com>
2025-07-27 20:10:13 +0700

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Nanda Mochammad on 27/07/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
