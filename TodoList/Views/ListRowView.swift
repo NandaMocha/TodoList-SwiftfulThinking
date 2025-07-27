959c24f8790702c51ffd9ee74e50dc7116b8815b
Nanda Mochammad <nanda.rosyadi@noble-software.com>
2025-07-27 20:29:01 +0700

    Create a List of Todo items and Add List View https://www.youtube.com/watch?v=EPdivac0kwE

---
+++ TodoList/Views/ListRowView.swift
@@ -0,0 +1,18 @@
+//
+//  ListRowView.swift
+//  TodoList
+//
+//  Created by Nanda Mochammad on 27/07/25.
+//
+
+import SwiftUI
+
+struct ListRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListRowView()
+}
---
+++ TodoList/Views/ListView.swift
@@ -0,0 +1,18 @@
+//
+//  ListView.swift
+//  TodoList
+//
+//  Created by Nanda Mochammad on 27/07/25.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListView()
+}
