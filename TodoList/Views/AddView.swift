f5b2867c28e2ebfb403b76f5ed9a448275f603c3
Nanda Mochammad <nanda.mocha@gmail.com>
2025-10-31 14:23:58 +0700

    feat: change copywriting to english

--- TodoList/Views/AddView.swift
+++ TodoList/Views/AddView.swift
@@ -17,7 +17,7 @@ struct AddView: View {
     
     var body: some View {
         ScrollView {
-            TextField("Ketik disini", text: $textFieldValue)
+            TextField("Type here", text: $textFieldValue)
                 .padding(.horizontal)
                 .frame(height: 55)
                 .background(
